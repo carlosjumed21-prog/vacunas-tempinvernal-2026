a032ad7516236817672d0ce14f452d76eebb3c0d
carlosjumed21-prog <carlosju.med21@gmail.com>
2026-09-19 16:57:29 -0600

    Update card display text in app.py

--- app.py
+++ app.py
@@ -664,7 +664,7 @@ st.markdown(
     unsafe_allow_html=True,
 )
 st.markdown(
-    f'<div class="card-grupo">🎯 {grupo_sugerido}</div>', unsafe_allow_html=True
+    f'<div class="card-grupo">🎯POR DEFINIR {grupo_sugerido}</div>', unsafe_allow_html=True
 )
 
 st.markdown("---")
